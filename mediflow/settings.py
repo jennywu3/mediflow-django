cd394e938cd78ec872b59302d2a68771c51e3e4f
Jenny <jennyccw3@gmail.com>
2025-07-21 15:07:09 +0200

    fix

--- mediflow/settings.py
+++ mediflow/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "mediflow.urls"
@@ -67,7 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "mediflow.wsgi.application"
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
